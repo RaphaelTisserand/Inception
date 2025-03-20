476242bf5628d4673c75e5eb1312d352861de495
raph2leretour <tisserandraph@gmail.com>
2025-03-20 17:09:51 +0100

    saaamy

--- srcs/requirements/mariadb/tools/myscript.sh
+++ srcs/requirements/mariadb/tools/myscript.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
 
-systemctl enable mariadb
-mysql_secure_installation --port=3306
+mkdir -p /run/mysqld/
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+
+if [ ! -f /var/lib/mysql/.installed ]; then
+	echo "Launching install script"
+	mariadb-install-db --user mysql
+	echo "Creating wordpress db and admin user"
+	mariadbd --bootstrap --user mysql <<EOF
+FLUSH PRIVILEGES;
+CREATE DATABASE $MARIADB_DBNAME;
+CREATE USER "$MARIADB_USER"@'%' IDENTIFIED BY "$MARIADB_PASSWORD";
+GRANT ALL PRIVILEGES ON $MARIADB_DBNAME.* TO "$MARIADB_USER"@'%' IDENTIFIED BY "$MARIADB_PASSWORD";
+FLUSH PRIVILEGES;
+exit
+EOF
+	touch /var/lib/mysql/.installed
+else
+	echo "mariadb is already installed, skipping installation.."
+fi
+
+echo "starting mariadbd server"
+exec mariadbd --user mysql
--- srcs/requirements/wordpress/tools/myscript.sh
+++ srcs/requirements/wordpress/tools/myscript.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
-echo "Hello World!"
+mkdir -p /run/php /var/www/html
+
+if [ ! -f /var/www/html/.installed ]; then
+	cp /usr/bin/php82 /usr/bin/php
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp-cli.phar
+	cd /var/www/html
+	echo "downloading wordpress"
+	wp-cli.phar core download
+	echo "creating wordpress config"
+	wp-cli.phar config create --dbname=$MARIADB_DBNAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb
+	echo "installing wordpress config"
+	wp-cli.phar core install --url=$DOMAIN_NAME --title=Inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+	wp-cli.phar user create $WP_USER $WP_EMAIL --role=subscriber --user_pass=$WP_PASSWORD --allow-root
+	wp-cli.phar theme install twentytwentythree --activate --allow-root
+	wp-cli.phar post delete $(wp-cli.phar post list --format=ids --allow-root) --allow-root
+	wp-cli.phar post create --post_type=post --post_title="This is a post" --post_content="... and its content" --post_status=publish --allow-root
+	touch /var/www/html/.installed
+else
+	echo "wordpress is already installed, skipping installation.."
+fi
+
+echo "starting php fpm"
+exec php-fpm82 -F -R
