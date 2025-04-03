320aa11f60d98a1b77f52fccc0b2570d0ff816c8
Raphael-Tisserand <tisserandraph@gmail.com>
2025-04-03 17:58:49 +0200

    ...samy naji

--- srcs/requirements/wordpress/tools/myscript.sh
+++ srcs/requirements/wordpress/tools/myscript.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
-sleep 20
-
 mkdir -p /run/php var/run/php /var/www/html
+
+sleep 5
 
 if [ -f /var/www/html/.installed ]; then
 	echo "Wordpress aleady installed."
 else
-	cp /usr/bin/php /usr/bin/php
+	cd /var/www/html
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp-cli.phar
-	cd /var/www/html
 	wp-cli.phar core download --allow-root
 	wp-cli.phar config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=3306 --allow-root
 	wp-cli.phar core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
 	wp-cli.phar user create $WP_USER $WP_EMAIL --role=subscriber --user_pass=$WP_PASSWORD --allow-root
+	wp theme install twentyfourteen --activate --allow-root
+	wp post delete $(wp post list --format=ids --allow-root) --allow-root
+	wp post create --post_type=post --post_title="This is a post" --post_content="... and its content :)" --post_status=publish --allow-root
 	touch /var/www/html/.installed
 fi
+
+chown -R www-data:www-data /var/www/inception/
+chmod -R 755 /var/www*
 
 exec /usr/sbin/php-fpm7.4 -F
