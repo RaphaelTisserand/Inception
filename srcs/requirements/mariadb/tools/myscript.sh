3bc4c2de861ddb78bf0bd6053f7ff4aad9308b8b
Raphael-Tisserand <tisserandraph@gmail.com>
2025-04-15 17:23:45 +0200

    Thea time

--- srcs/requirements/mariadb/tools/myscript.sh
+++ srcs/requirements/mariadb/tools/myscript.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" >>/etc/mysql/init.sql
-echo "CREATE USER IF NOT EXISTS $SQL_USER@'localhost' IDENTIFIED BY '$SQL_PASSWORD';" >>/etc/mysql/init.sql
+echo "CREATE USER IF NOT EXISTS $SQL_USER@'*' IDENTIFIED BY '$SQL_PASSWORD';" >>/etc/mysql/init.sql
 echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER@'localhost' IDENTIFIED BY '$SQL_PASSWORD';" >>/etc/mysql/init.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';" >>/etc/mysql/init.sql
+echo "ALTER USER 'root'@'*' IDENTIFIED BY '$SQL_ROOT_PASSWORD';" >>/etc/mysql/init.sql
 echo "FLUSH PRIVILEGES;" >>/etc/mysql/init.sql
 
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/myscript.sh
+++ srcs/requirements/wordpress/tools/myscript.sh
@@ -1,43 +1,36 @@
 #!/bin/sh
 
-sleep 10
+cd /var/www/html/
 
-if [ -f /var/www/html/wp-config.php ]; then
-	echo "Wordpress aleady installed."
-else
+rm -f wp-config.php
 
-	cd /var/www/html
+wp config create \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASSWORD \
+	--dbhost=mariadb:3360 \
+	--skip-check \
+	--allow-root \
+	--path='/var/www/html'
 
-	wp core download --allow-root
-	rm -f /var/www/html/wp-config.php
-	wp config create \
-		--dbname=$SQL_DATABASE \
-		--dbuser=$SQL_USER \
-		--dbpass=$SQL_PASSWORD \
-		--dbhost=mariadb \
-		--skip-check
-	wp core install \
-		--url=$DOMAIN_NAME \
-		--title=$SITE_TITLE \
-		--admin_user=$WP_ADMIN_USER \
-		--admin_password=$WP_ADMIN_PASSWORD \
-		--admin_email=$WP_ADMIN_EMAIL \
-		--allow-root
-	wp user create $WP_USER $WP_EMAIL \
-		--role=suscriber \
-		--user_pass=$WP_PASSWORD \
-		--allow-root
-	wp theme install twentyfourteen \
-		--activate \
-		--allow-root
-	wp post delete $(wp post list --format=ids --allow-root) \
-		--allow-root
-	wp post create \
-		--post_type=post \
-		--post_title="This is a post" \
-		--post_content="... and its content :)" \
-		--post_status=publish \
-		--allow-root
-fi
+until wp db check --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --path=/var/www/html --quiet --allow-root; do
+	echo "Waiting for MySQL..."
+	sleep 1
+done
+
+wp core install \
+	--url=$DOMAIN_NAME \
+	--title=$SITE_TITLE \
+	--admin_user=$WP_ADMIN_USER \
+	--admin_password=$WP_ADMIN_PASSWORD \
+	--admin_email=$WP_ADMIN_EMAIL \
+	--allow-root \
+	--path='/var/www/html'
+
+wp user create $WP_USER $WP_EMAIL \
+	--role=suscriber \
+	--user_pass=$WP_PASSWORD \
+	--allow-root \
+	--path='/var/www/html'
 
 exec /usr/sbin/php-fpm7.4 -F
