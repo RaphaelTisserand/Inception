ca69029b17e48cbb22cf7cde2bccb11fb8142033
Raphael-Tisserand <tisserandraph@gmail.com>
2025-04-15 14:31:36 +0200

    J’ai encore faim putain

--- srcs/requirements/wordpress/tools/myscript.sh
+++ srcs/requirements/wordpress/tools/myscript.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 
-cd 
+sleep 10
 
 if [ -f /var/www/html/wp-config.php ]; then
 	echo "Wordpress aleady installed."
 else
 
-	sleep 15
-
 	cd /var/www/html
 
-	wp core download \
-		--allow-root
+	wp core download --allow-root
 	rm -f /var/www/html/wp-config.php
 	wp config create \
 		--dbname=$SQL_DATABASE \
 		--dbuser=$SQL_USER \
 		--dbpass=$SQL_PASSWORD \
-		--dbhost=mariadb:9000 \
-		--skip-check \
-		--allow-root
+		--dbhost=mariadb \
+		--skip-check
 	wp core install \
 		--url=$DOMAIN_NAME \
 		--title=$SITE_TITLE \
@@ -30,6 +26,17 @@ else
 	wp user create $WP_USER $WP_EMAIL \
 		--role=suscriber \
 		--user_pass=$WP_PASSWORD \
+		--allow-root
+	wp theme install twentyfourteen \
+		--activate \
+		--allow-root
+	wp post delete $(wp post list --format=ids --allow-root) \
+		--allow-root
+	wp post create \
+		--post_type=post \
+		--post_title="This is a post" \
+		--post_content="... and its content :)" \
+		--post_status=publish \
 		--allow-root
 fi
 
